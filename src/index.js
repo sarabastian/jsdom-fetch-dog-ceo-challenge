a30fe3a00f1d1bc6f10eacc809dd4284f1ed7a54
sarabastian <seb118@georgetown.edu>
2020-11-25 08:32:58 -0500

    commit tree 3ffb9b2d0f4739916c02794c1d2c167248f6d3fa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+
+document.addEventListener('DOMContentHasLoaded', () => {
+    fetchDogImages()
+})
+
+function fetchDogImages() {
+    fetch(imgUrl)
+    .then(r => r.json())
+    .then(json => {
+        console.log(json.message)
+        renderDogImages(json.message)
+    })
+}
+
+function renderDogImages(images) {
+    for(let i = 0; i < images.length; i++) {
+        const container = document.getElementById('dog-image-container')
+        container.innerHTML.src = images[i]
+    }
+        
+    };
